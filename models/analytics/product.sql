0e92103d05fcd55ae171e10092b86070c3580416
kivisa <noinung4@gmail.com>
2024-01-06 08:04:55 +0700

    add 2 columns

--- models/analytics/product.sql
+++ models/analytics/product.sql
@@ -1,5 +1,9 @@
-SELECT stock_item_id as product_id
-      , stock_item_name as product_name
-      , coalesce(brand, 'Undefined') as brand 
-      , supplier_id
-      FROM `vit-lam-data.wide_world_importers.warehouse__stock_items` 
+SELECT product.stock_item_id as product_id
+      , product.stock_item_name as product_name
+      , coalesce(product.brand, 'Undefined') as brand 
+      , product.supplier_id
+      ,supplier.supplier_name
+      FROM `vit-lam-data.wide_world_importers.warehouse__stock_items` as product
+      join `learn-dbt-410122.learn_dbt.supplier` as supplier
+      on product.supplier_id = supplier.supplier_id
+      
