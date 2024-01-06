86c232b3eaa4731a8a99a5548abc4aae200ced3e
kivisa <noinung4@gmail.com>
2024-01-06 08:08:54 +0700

    add ref('supplier')

--- models/analytics/product.sql
+++ models/analytics/product.sql
@@ -4,6 +4,6 @@ SELECT product.stock_item_id as product_id
       , product.supplier_id
       ,supplier.supplier_name
       FROM `vit-lam-data.wide_world_importers.warehouse__stock_items` as product
-      join `learn-dbt-410122.learn_dbt.supplier` as supplier
+      join {{ ref('supplier') }} as supplier
       on product.supplier_id = supplier.supplier_id
       
--- models/analytics/supplier.sql
+++ models/analytics/supplier.sql
@@ -1,2 +1,4 @@
-select supplier_id, upper(supplier_name) as supplier_name , supplier_category_id 
+select supplier_id
+      , upper(supplier_name) as supplier_name 
+      , supplier_category_id 
 from `vit-lam-data.wide_world_importers.purchasing__suppliers`
